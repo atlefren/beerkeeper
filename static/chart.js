aae14241a0a1fe762cf61971821b5012cbb8b60f
Atle Frenvik Sveen <atle.sveen@gmail.com>
2014-10-28 23:04:57 +0100

    cleanup

--- static/chart.js
+++ static/chart.js
@@ -1,10 +1,9 @@
 var BK = this.BK || {};
 (function (ns) {
-    
     'use strict';
 
+
     function createGraphData(dict) {
-        console.log(dict);
         var beerData = _.map(dict, function (value, key) {
             return {x: moment(key).valueOf(), y: value.beers};
         });
@@ -25,18 +24,16 @@ var BK = this.BK || {};
         ];
     };
 
-    ns.printGraph = function (dict) {
 
-        /*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
+    ns.printGraph = function (dict, elementId) {
         nv.addGraph(function() {
             var chart = nv.models.lineChart()
-                    .margin({left: 100, right: 100})  //Adjust chart margins to give the x-axis some breathing room.
-                    .useInteractiveGuideline(false)  //We want nice looking tooltips and a guideline!
-                    .transitionDuration(350)  //how fast do you want the lines to transition?
-                    .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
-                    .showYAxis(true)        //Show the y-axis
-                    .showXAxis(true)        //Show the x-axis
-            ;
+                    .margin({left: 100, right: 100})
+                    .useInteractiveGuideline(false)
+                    .transitionDuration(350)
+                    .showLegend(true)
+                    .showYAxis(true)
+                    .showXAxis(true);
 
             chart.xAxis
                 .axisLabel('Date')
@@ -44,20 +41,16 @@ var BK = this.BK || {};
                     return d3.time.format('%d %b %Y')(new Date(d));
                 });
 
-            chart.yAxis     //Chart y-axis settings
+            chart.yAxis
                 .tickFormat(d3.format('.02f'));
 
-            /* Done setting the chart up? Time to render it!*/
+            d3.select('#' + elementId)
+                .datum(createGraphData(dict))
+                .call(chart);
 
-            d3.select('#chart svg')    //Select the <svg> element you want to render the chart in.   
-                .datum(createGraphData(dict))         //Populate the <svg> element with chart data...
-                .call(chart);          //Finally, render the chart!
-
-            //Update the chart when window resizes.
             nv.utils.windowResize(function() {
                 chart.update();
             });
-            return chart;
         });
     };
 
